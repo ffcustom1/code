2e4b4bf5fcc04c6e79e07dde0d70a0e733abc20c
ffcustom1 <150421799+ffcustom1@users.noreply.github.com>
2024-05-23 13:52:05 +0530

    Create download.ps1

---
+++ download.ps1
@@ -0,0 +1,6 @@
+# download.ps1
+
+# Section: Installing Applications
+Write-Output "Installing applications using Chocolatey..."
+choco list 
+Write-Output "All installations completed successfully."
