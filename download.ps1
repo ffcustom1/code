1b62b6e3e5d0f10862280afb8399dcaa2b94e241
ffcustom1 <150421799+ffcustom1@users.noreply.github.com>
2024-05-23 13:55:30 +0530

    Update download.ps1

--- download.ps1
+++ download.ps1
@@ -2,5 +2,10 @@
 
 # Section: Installing Applications
 Write-Output "Installing applications using Chocolatey..."
-choco list 
+choco install yt-dlp ffmpeg onedrive brave gallery-dl
+
+# Section: Installing Python Extension
+Write-Output "Installing Hanime TV plugin for Python..."
+python3 -m pip install -U https://github.com/cynthia2006/hanime-tv-plugin/archive/master.zip
+
 Write-Output "All installations completed successfully."
